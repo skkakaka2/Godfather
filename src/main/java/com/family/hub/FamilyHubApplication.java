0c6f050a7c9b66b457723a607f82fb1f6d6eef07
wangzeyu <wangzeyu_sybq@kedacom.com>
2026-05-10 00:10:54 +0800

    date

--- src/main/java/com/family/hub/FamilyHubApplication.java
+++ src/main/java/com/family/hub/FamilyHubApplication.java
@@ -1,14 +1,22 @@
 package com.family.hub;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class FamilyHubApplication {
 
     public static void main(String[] args) {
+
+        log.info("Application started at {}", LocalDateTime.now());
+
         SpringApplication.run(FamilyHubApplication.class, args);
     }
 }
