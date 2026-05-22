c86167a868587fb9ffd23376192d62a659d13f69
zeyu.wang <zeyu.wang@dyness-tech.com>
2026-05-22 21:14:01 +0800

    域名调整

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -1,4 +1,4 @@
 const DEV_API_BASE_URL = 'http://localhost:6006';
-const PROD_API_BASE_URL = 'http://raspberrypi:8081';
+const PROD_API_BASE_URL = 'http://raspberrypi.tailf9603e.ts.net:8081';
 
 export const API_BASE_URL = __DEV__ ? DEV_API_BASE_URL : PROD_API_BASE_URL;
