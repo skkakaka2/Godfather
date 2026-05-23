bb4cdb96fc7ac64a464b86b923bfee9b77fc0019
zeyu.wang <zeyu.wang@dyness-tech.com>
2026-05-23 13:43:50 +0800

    kong

--- mobile/src/config/env.ts
+++ mobile/src/config/env.ts
@@ -1,4 +1,4 @@
 const DEV_API_BASE_URL = 'http://localhost:6006';
-const PROD_API_BASE_URL = 'http://raspberrypi.tailf9603e.ts.net:8081';
+const PROD_API_BASE_URL = 'https://raspberrypi.tailf9603e.ts.net:8443';
 
 export const API_BASE_URL = __DEV__ ? DEV_API_BASE_URL : PROD_API_BASE_URL;
